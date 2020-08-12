e3fffbd749720aebf41d14dd9951bc1c803410a2
Matt Meleen <mmeleen@utexas.edu>
2020-08-11 19:15:27 -0500

    Modified cache file list

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,8 @@ const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/index.js',
-  '../models/transaction.js',
   '/manifest.webmanifest',
-  '/assets/css/styles.css',
+  '/styles.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'
 ];
